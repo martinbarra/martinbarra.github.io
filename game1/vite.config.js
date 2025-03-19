ea2d486cda11a56b5300862bf8e1716f7bd14380
martinbarra <69089716+martinbarra@users.noreply.github.com>
2025-03-19 16:03:28 -0300

    zxcv

--- game1/vite.config.js
+++ game1/vite.config.js
@@ -1,5 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: "./", // Esto corrige las rutas en GitHub Pages
+  base: './', // Esto es importante para GitHub Pages
+  build: {
+    outDir: 'dist', // Especificamos que la salida debe ir a la carpeta "dist"
+    assetsDir: 'assets', // Los archivos estáticos, como imágenes y otros recursos, estarán dentro de "dist/assets"
+    rollupOptions: {
+      input: './index.html', // Aseguramos que Vite utilice el index.html correcto
+    },
+  },
 });
